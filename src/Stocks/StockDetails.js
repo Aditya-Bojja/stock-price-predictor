876d96ddea64b5a40bd45b78e79942576725b6db
BOJJA ADITYA <baditya2070@gmail.com>
2022-05-30 20:21:05 +0530

    jkl

--- src/Stocks/StockDetails.js
+++ src/Stocks/StockDetails.js
@@ -153,54 +153,20 @@ function StockDetails(){
 
         setRenderFutureGraph(true);
         let baseURL = `http://mukesh.southindia.cloudapp.azure.com/predict?Company=${currentSymbol}`;
-
-        if(currentSymbol === "IBM"){
-            let tempValues = [1, 3, -1, 4, 3];
-            fetch(baseURL, {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json', 
-                }
-            }).then(response => response.json()).then(data => {
-                let tempArray = [];
-                for(let j = 1; j < 6; j++){
-                    tempArray.unshift(Number(data[j]) + 42 + tempValues[j - 1]);
-                }
-                setFutureClosePrice(tempArray.concat([].fill(0,0,25)));
-                console.log("Received data" ,  tempArray);
-                // console.log("Future Price DATA: ", futureClosePrice);
-            });
-        } else if(currentSymbol === "TSLA"){
-            fetch(baseURL, {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json', 
-                }
-            }).then(response => response.json()).then(data => {
-                let tempArray = [];
-                for(let j = 1; j < 6; j++){
-                    tempArray.unshift(Number(data[j]) + 580);
-                }
-                setFutureClosePrice(tempArray.concat([].fill("",0,25)));
-                console.log("Received data" ,  tempArray);
-                // console.log("Future Price DATA: ", futureClosePrice);
-            });
-        } else {
-            fetch(baseURL, {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json', 
-                }
-            }).then(response => response.json()).then(data => {
-                let tempArray = [];
-                for(let j = 1; j < 6; j++){
-                    tempArray.unshift(data[j]);
-                }
-                setFutureClosePrice(tempArray.concat([].fill("",0,25)));
-                console.log("Received data" ,  tempArray);
-                // console.log("Future Price DATA: ", futureClosePrice);
-            });
-        }
+ 
+        fetch(baseURL, {
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json', 
+            }
+        }).then(response => response.json()).then(data => {
+            let tempArray = [];
+            for(let j = 1; j < 6; j++){
+                tempArray.unshift(data[j]);
+            }
+            setFutureClosePrice(tempArray.concat([].fill("",0,25)));
+            console.log("Received data" ,  tempArray);
+        });
 
         let futureDates = [];
         var today = new Date();
